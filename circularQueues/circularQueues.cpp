f1d1a7e392567f9a976576b23a459977962a2a56
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-23 15:12:23 +0700

    jika ya set = 0, else front + 1

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -53,8 +53,10 @@ public:
 		}
 		else {
 			//jika elemen yang dihapus berada di posisi akhir array, kembali ke awal array
-			if (FRONT == max - 1) 
-
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
 		}
 	}
 };
