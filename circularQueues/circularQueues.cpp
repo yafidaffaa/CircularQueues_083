9c6b6a932d6d85a75cbfece7ca064f39108bd60d
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-23 15:32:39 +0700

    pengulangan display dari awal array hingga rear

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -91,7 +91,13 @@ public:
 
 			FRONT_position = 0;
 
-
+			//Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) 
+			{
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
