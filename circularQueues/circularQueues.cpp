0eba9c64e5671aaa8e69cbdbfecb5f7cdddc7748
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-23 15:23:43 +0700

    cek kondisi dan perulangan selama front <= rear

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -72,6 +72,13 @@ public:
 
 		cout << "\nElement in the queue are...\n";
 
-
+		// Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position)
+			{
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+		}
 	}
 };
