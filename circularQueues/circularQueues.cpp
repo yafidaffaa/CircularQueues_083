720807122cc98774c8ae560524c0f1f9253b82b3
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-23 15:27:06 +0700

    perulangan untuk menampilkan dari FRONT hingga akhir array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -79,6 +79,19 @@ public:
 				cout << queue_array[FRONT_position] << "  ";
 				FRONT_position++;
 			}
+			cout << endl;
+		}
+		else {
+			//Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) 
+			{
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+
 		}
 	}
 };
